416361a4fce9d516fd0dca3f069e1b8e1f9ce7ff
Lev <leozolotukhin@gmail.com>
2024-08-15 11:41:00 +0200

    Update parseSortParams.js

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,4 +1,4 @@
-import { SORT_ORDER } from '../constants/index.js';
+import { SORT_ORDER } from '../constans/index.js';
 
 const parseSortOrder = (sortOrder) => {
   const isKnownOrder = [SORT_ORDER.ADS, SORT_ORDER.DESC].includes(sortOrder);
